8ec09090fb160a5ad5855171dbb9971bebea077f
Vatsal <vatsal.shah2304@gmail.com>
2025-06-02 11:16:03 +0530

    debugging deployement

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const gameSocketManager = new GameSocketManager(server);
 
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public"), { index: 'index.html' }));
 app.use(cors());
 
 app.get('/', (req, res) => {
